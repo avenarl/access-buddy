963e88e0c3366da199e3f1433da52878f957e7d0
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-04-01 15:59:46 +0800

    :on: update(routing): add create link and enable routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserCreateComponent } from './user-create/user-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'create', component: UserCreateComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
