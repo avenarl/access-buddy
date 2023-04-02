f4a355f19ad49abee8fdc518aa7b033ff25e2bc1
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-04-02 09:43:41 +0800

    :heavy_plus_sign: update(component): set method to user-list

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { User } from '../user.model';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
 })
 export class UserListComponent {
+  user: User[] = [];
+  constructor(private userService: UserService) {}
 
+  getUsers(): void {
+    this.user = this.userService.getUsers();
+  }
 }
