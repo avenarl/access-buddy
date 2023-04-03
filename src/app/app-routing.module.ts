8cdf2a3f000d77dbb7d143ca960090beb4355f8b
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-04-03 10:37:11 +0800

    :heavy_plus_sign: update(route): add route user-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserCreateComponent } from './user-create/user-create.component';
+import { UserListComponent } from './user-list/user-list.component';
 
-const routes: Routes = [{ path: 'create', component: UserCreateComponent }];
+const routes: Routes = [
+  { path: '', component: UserListComponent },
+  { path: 'create', component: UserCreateComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
