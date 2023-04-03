7caf9937dedcd17bde3c847e4c3a43ca16710ff9
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-04-03 15:25:25 +0800

    :heavy_plus_sign: update(component): add data array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { User } from './user.model';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'access-buddy';
+
+  userList: UserService[] = [
+    {
+      firstName: 'Juan',
+      lastName: 'Dela Cruz',
+      birthDate: '01-01-1990',
+      gender: 'Male',
+      email: 'juan@hotmail.com',
+      mobileNumber: '09123456789',
+      address: 'Manila, Philippines',
+    },
+  ];
 }
