b0308d7d648691a913471838051d718813690314
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-04-05 10:52:45 +0800

    :heavy_plus_sign: update(module): add reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,7 @@ import { UserService } from './user.service';
 
 @NgModule({
   declarations: [AppComponent, UserCreateComponent, UserListComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [UserService],
   bootstrap: [AppComponent],
 })
