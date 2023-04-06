850b83c46467dd03bcfb8250b11177754a3fe058
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-04-07 00:26:59 +0800

    :heavy_plus_sign: update(service): create method with localstorage

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -5,8 +5,29 @@ import { User } from './user.model';
   providedIn: 'root',
 })
 export class UserService {
-  users: User[] = [];
-  constructor() {}
+  private users: User[] = [];
+  constructor() {
+    this.loadFromLocalStorage(); // use as backend
+  }
+
+  loadFromLocalStorage() {
+    // Get saved users from localStorage as JSON string
+    const savedUsers = localStorage.getItem('users');
+
+    // Validate if there is data in localStorage
+    if (savedUsers) {
+      // Parse the JSON string and assign to this.users
+      this.users = JSON.parse(savedUsers);
+    } else {
+      // assign empty array to this.users
+      this.users = [];
+    }
+  }
+
+  // save users array to storage as JSON string
+  saveToLocalStorage() {
+    localStorage.setItem('users', JSON.stringify(this.users));
+  }
 
   createUser(users: any) {
     this.users.push(users);
