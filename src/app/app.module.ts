b799f57cbec3c8cb68bc455f81b5bcf4a4b3007a
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-04-07 21:39:39 +0800

    :heavy_plus_sign: update(module): set paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,29 @@ import { UserCreateComponent } from './user-create/user-create.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserService } from './user.service';
 import { UserEditComponent } from './user-edit/user-edit.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  { path: 'users', component: UserListComponent },
+  { path: 'users/create', component: UserCreateComponent },
+  { path: 'users/edit/:id', component: UserEditComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent, UserCreateComponent, UserListComponent, UserEditComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  declarations: [
+    AppComponent,
+    UserCreateComponent,
+    UserListComponent,
+    UserEditComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
+  ],
   providers: [UserService],
   bootstrap: [AppComponent],
 })
