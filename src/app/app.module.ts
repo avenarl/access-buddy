bad1e6793696ad559b8da2ac447d264e66f3516d
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-04-12 14:42:38 +0800

    :star: feat(component): create delete component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { UserCreateComponent } from './user-create/user-create.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserService } from './user.service';
 import { UserEditComponent } from './user-edit/user-edit.component';
+import { UserDeleteComponent } from './user-delete/user-delete.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { UserEditComponent } from './user-edit/user-edit.component';
     UserCreateComponent,
     UserListComponent,
     UserEditComponent,
+    UserDeleteComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [UserService],
