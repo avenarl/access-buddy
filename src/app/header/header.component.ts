9067abb93924762b1571b6eee2442d46c966eda2
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-04-15 17:42:08 +0800

    :star: feat(component): add header with login button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
+  constructor(public userService: UserService) {}
 }
