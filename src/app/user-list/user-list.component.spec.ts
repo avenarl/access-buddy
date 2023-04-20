49416753b08493e1c022b35bfbc335d24a85157a
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-04-20 22:07:15 +0800

    :heavy_check_mark: test(component): should populate users

--- src/app/user-list/user-list.component.spec.ts
+++ src/app/user-list/user-list.component.spec.ts
@@ -2,11 +2,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './user-list.component';
 import { UserService } from '../user.service';
+import { User } from '../user.model';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;
-  let UserService: UserService;
+  let userService: UserService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -26,8 +27,30 @@ describe('UserListComponent', () => {
   });
 
   it('should call getUsers()', () => {
-    const getUsersSpy = spyOn(UserService, 'getUsers').and.callThrough();
+    const getUsersSpy = spyOn(userService, 'getUsers').and.callThrough();
     component.ngOnInit();
     expect(getUsersSpy).toHaveBeenCalled();
+  });
+
+  it('should populate users property with data from getUsers()', () => {
+    const currentUsers: User[] = [
+      {
+        id: 1681649188558,
+        firstName: 'admin',
+        lastName: 'admin',
+        birthDate: new Date('01/01/1990'),
+        gender: 'male',
+        email: 'admin@example.com',
+        mobileNumber: 9123456789,
+        address: 'Philippines',
+        password: 'admin',
+        role: 'admin',
+      },
+    ];
+
+    spyOn(userService, 'getUsers').and.returnValue(currentUsers);
+    component.userService = userService;
+    component.ngOnInit();
+    expect(component.users).toEqual(currentUsers);
   });
 });
