24f69665b7258bb6b959564b8e20f370fd560cbc
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-04-20 20:19:57 +0800

    :white_check_mark: test(component): should call getUsers method

--- src/app/user-list/user-list.component.spec.ts
+++ src/app/user-list/user-list.component.spec.ts
@@ -1,22 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './user-list.component';
+import { UserService } from '../user.service';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;
+  let UserService: UserService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [UserListComponent],
+      providers: [UserService],
     }).compileComponents();
-
+  });
+  beforeEach(() => {
     fixture = TestBed.createComponent(UserListComponent);
     component = fixture.componentInstance;
+    userService = TestBed.inject(UserService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getUsers()', () => {
+    const getUsersSpy = spyOn(UserService, 'getUsers').and.callThrough();
+    component.ngOnInit();
+    expect(getUsersSpy).toHaveBeenCalled();
   });
 });
