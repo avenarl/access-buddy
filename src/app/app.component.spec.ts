6e9e3cdf73fc569d99aa691db74785ee7686dcc6
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-04-20 22:26:29 +0800

    :heavy_check_mark: test(component): shound have and render title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'access-buddy'`, () => {
+  it(`should have as title 'Access Buddy'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('access-buddy');
+    expect(app.title).toEqual('Access Buddy');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('access-buddy app is running!');
+    expect(compiled.querySelector('header h1')?.textContent).toContain(
+      'Access Buddy'
+    );
   });
 });
