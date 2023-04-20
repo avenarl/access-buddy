a921306d7356c69834cd59bbc4534fba61dbd8f9
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-04-21 00:05:12 +0800

    :heavy_check_mark: test(component): fix activated route in edit

--- src/app/user-edit/user-edit.component.spec.ts
+++ src/app/user-edit/user-edit.component.spec.ts
@@ -2,15 +2,25 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserEditComponent } from './user-edit.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { convertToParamMap } from '@angular/router';
 
 describe('UserEditComponent', () => {
   let component: UserEditComponent;
   let fixture: ComponentFixture<UserEditComponent>;
 
+  const mockActivatedRoute = {
+    snapshot: {
+      paramMap: convertToParamMap({ id: '1' }),
+    },
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [UserEditComponent],
       imports: [ReactiveFormsModule],
+      providers: [{ provide: ActivatedRoute, useValue: mockActivatedRoute }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(UserEditComponent);
