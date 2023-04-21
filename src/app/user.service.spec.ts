bab3cb95c482190adc7b9ea32ab974916d810148
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-04-21 23:19:00 +0800

    :heavy_check_mark: test(component): able to delete a user

--- src/app/user.service.spec.ts
+++ src/app/user.service.spec.ts
@@ -1,16 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
+import { User } from './user.model';
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
+    userService = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(userService).toBeTruthy();
+  });
+
+  it('should delete a user', () => {
+    spyOn(userService, 'deleteUser').and.callThrough();
+
+    userService.deleteUser(1);
+    expect(userService.deleteUser).toHaveBeenCalledWith(1);
   });
 });
