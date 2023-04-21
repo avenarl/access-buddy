1490c1251a09e5738cc798f487dbf3b513e87f88
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-04-21 23:23:37 +0800

    :heavy_check_mark: test(component): able to create user

--- src/app/user.service.spec.ts
+++ src/app/user.service.spec.ts
@@ -21,4 +21,23 @@ describe('UserService', () => {
     userService.deleteUser(1);
     expect(userService.deleteUser).toHaveBeenCalledWith(1);
   });
+
+  it('should create a user', () => {
+    spyOn(userService, 'createUser').and.callThrough();
+    const newUser: User = {
+      id: 1,
+      firstName: 'John',
+      lastName: 'Doe',
+      birthDate: new Date('1990-01-01'),
+      gender: 'male',
+      email: 'john.doe@example.com',
+      mobileNumber: 9123456789,
+      address: '123 Main St',
+      password: 'password',
+      role: 'user',
+    };
+
+    userService.createUser(newUser);
+    expect(userService.createUser).toHaveBeenCalledWith(newUser);
+  });
 });
